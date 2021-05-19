62d8b36597a244c648bb4bf71295574b759c7aba
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-20 02:36:29 +0300

    Entities and Core Layers created!

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using System;
+
+namespace Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Nicename { get; set; }
+        public string DisplayName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Registered { get; set; }
+        public string Url { get; set; }
+        public string Description { get; set; }
+        public bool Status { get; set; }
+    }
+}
