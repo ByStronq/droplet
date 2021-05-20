ab709c3f5c64c47925607ba70893eac746328644
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-20 03:10:15 +0300

    DataAccess Layer created and EntityFrameworkCore.SqlServer module added!

---
+++ DataAccess/Concrete/EntityFramework/DropletContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class DropletContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=.;Database=Droplet;Trusted_Connection=true");
+        }
+    }
+}
