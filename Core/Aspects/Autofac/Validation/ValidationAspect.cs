6700d6d5b9798fb1e275b01814cd648fcfd7fed8
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-21 02:53:30 +0300

    ValidationAspect moved!

--- Core/Aspects/Autofac/Validation/ValidationAspect.cs
+++ Core/Aspects/Autofac/Validation/ValidationAspect.cs
@@ -5,7 +5,7 @@ using FluentValidation;
 using System;
 using System.Linq;
 
-namespace Core.Aspects.Validation
+namespace Core.Aspects.Autofac.Validation
 {
     public class ValidationAspect : MethodInterception
     {
