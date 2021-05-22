e912de937dcdc705ee4d8426648bb4837b095034
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-22 04:21:30 +0300

    SecuredOperationAspect!

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,13 @@
+﻿namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string AuthorizationDenied => "Yetkilendirme Reddedildi";
+        public static string UserRegistered => "Kullanıcı kayıt oldu";
+        public static string UserNotFound => "Kullanıcı bulunamadı";
+        public static string PasswordError => "Şifre hatası";
+        public static string SuccessfulLogin => "Giriş başarılı";
+        public static string UserAlreadyExists => "Kullanıcı zaten mevcut";
+        public static string AccessTokenCreated => "Erişim jetonu oluşturuldu";
+    }
+}
