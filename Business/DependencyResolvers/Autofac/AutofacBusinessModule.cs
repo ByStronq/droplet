cfae7034bc1fabfd87a6be5972d033ec49e9fd4e
CEMAL YILDIRIM <bystrong@technovadi.com>
2021-05-25 16:49:40 +0300

    Added IService interfaces' DependencyResolvers to Business Layer!

--- Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,8 +1,12 @@
 ﻿using Autofac;
 using Autofac.Extras.DynamicProxy;
+using Business.Abstract;
+using Business.Concrete;
 using Castle.DynamicProxy;
 using Core.Utilities.Interceptors;
 using Core.Utilities.Security.JWT;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
 
 namespace Business.DependencyResolvers.Autofac
 {
@@ -10,6 +14,39 @@ namespace Business.DependencyResolvers.Autofac
     {
         protected override void Load(ContainerBuilder builder)
         {
+            builder.RegisterType<CommentMetaManager>().As<ICommentMetaService>();
+            builder.RegisterType<EfCommentMetaDal>().As<ICommentMetaDal>();
+
+            builder.RegisterType<CommentManager>().As<ICommentService>();
+            builder.RegisterType<EfCommentDal>().As<ICommentDal>();
+
+            builder.RegisterType<LinkManager>().As<ILinkService>();
+            builder.RegisterType<EfLinkDal>().As<ILinkDal>();
+
+            builder.RegisterType<OptionManager>().As<IOptionService>();
+            builder.RegisterType<EfOptionDal>().As<IOptionDal>();
+
+            builder.RegisterType<PostMetaManager>().As<IPostMetaService>();
+            builder.RegisterType<EfPostMetaDal>().As<IPostMetaDal>();
+
+            builder.RegisterType<PostManager>().As<IPostService>();
+            builder.RegisterType<EfPostDal>().As<IPostDal>();
+
+            builder.RegisterType<TermMetaManager>().As<ITermMetaService>();
+            builder.RegisterType<EfTermMetaDal>().As<ITermMetaDal>();
+
+            builder.RegisterType<TermRelationshipManager>().As<ITermRelationshipService>();
+            builder.RegisterType<EfTermRelationshipDal>().As<ITermRelationshipDal>();
+
+            builder.RegisterType<TermManager>().As<ITermService>();
+            builder.RegisterType<EfTermDal>().As<ITermDal>();
+
+            builder.RegisterType<TermTaxonomyManager>().As<ITermTaxonomyService>();
+            builder.RegisterType<EfTermTaxonomyDal>().As<ITermTaxonomyDal>();
+
+            builder.RegisterType<UserMetaManager>().As<IUserMetaService>();
+            builder.RegisterType<EfUserMetaDal>().As<IUserMetaDal>();
+
             builder.RegisterType<JwtHelper>().As<ITokenHelper>();
 
             var assembly = System.Reflection.Assembly.GetExecutingAssembly();
